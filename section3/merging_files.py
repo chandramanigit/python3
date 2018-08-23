38facd053b61d53a5b41cee37a5bf853a346e3cb
chandramanigit <41801148+chandramanigit@users.noreply.github.com>
2018-08-23 21:51:20 +0530

    Add files via upload

--- section3/merging_files.py
+++ section3/merging_files.py
@@ -3,7 +3,7 @@ from datetime import datetime
 filenames = glob2.glob("file*.txt")  # listing file in directory
 now = datetime.now()  # creating time delta object
 temp=now.strftime("%Y-%m-%d-%H-%M-%S-%f")  # time delta object in string formate store in temp variable to be use as file name
-for f in filenames:
-    with open(temp+".txt" ,"a") as myfile:  #this do open a file to write with .txt extension
+with open(temp+".txt" ,"w") as myfile:  #this do open a file to write with .txt extension
+    for f in filenames:
         with open(f , "r") as tempf:  # this open file to read whcih we are going to wirte
-            myfile.write(tempf.read() + "\n") # final approach 
+            myfile.write(tempf.read() + "\n") # final approach
